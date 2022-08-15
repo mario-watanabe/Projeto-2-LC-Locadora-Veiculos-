1452d2e2e5d2026721516935e021a11b4e46f54b
mario-watanabe <109993307+mario-watanabe@users.noreply.github.com>
2022-08-15 20:38:45 -0300

    Create Desconto.java

---
+++ Desconto.java
@@ -0,0 +1,25 @@
+public class Desconto {
+    String tipoCliente;
+    int quantidadeDeDias;
+
+    public Desconto() {}
+
+    public Desconto(String tipoCliente, int quantidadeDeDias) {
+    }
+    
+    public double taxaDesconto(String tipoCliente, int quantidadeDeDias) {
+        this.tipoCliente = tipoCliente;
+        this.quantidadeDeDias = quantidadeDeDias;
+        double taxaDesconto = 0;
+        if(this.tipoCliente == "PF" && this.quantidadeDeDias > 5) {
+            taxaDesconto = 5;            
+        } else { if(this.tipoCliente == "PJ" && this.quantidadeDeDias > 3) {
+            taxaDesconto = 10;
+        }
+        }
+        return taxaDesconto;
+    }  
+        
+}
+    
+

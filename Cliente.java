f1b8be7a16a7e6d817268165f1d0e7e4f4dda783
mario-watanabe <109993307+mario-watanabe@users.noreply.github.com>
2022-08-15 13:00:24 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,30 @@
+public class Cliente {
+    String nome;
+    String tipoCliente;
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public Cliente(){}
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public static Cliente valueOf(String tipoCliente2) {
+        return null;
+    }
+}

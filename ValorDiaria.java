6aa6e32734902a57a803dc770ca7b812f0efbfcb
mario-watanabe <109993307+mario-watanabe@users.noreply.github.com>
2022-08-15 20:39:50 -0300

    Update ValorDiaria.java

--- ValorDiaria.java
+++ ValorDiaria.java
@@ -58,7 +58,40 @@ public class ValorDiaria extends Cliente {
               valorDiaria = new BigDecimal(135.00);
             }  else { 
               valorDiaria = new BigDecimal(150.00);
-            }
+            }import java.math.BigDecimal;
+
+public class ValorDiaria extends Cliente {
+   Veiculo tipoVeiculo;
+   BigDecimal valorDiaria;
+   String tipoCliente;
+   int quantidadeDeDias;
+
+   public ValorDiaria() {}
+
+   public ValorDiaria(Veiculo tipoVeiculo){}
+
+   public BigDecimal valorDiaria (Veiculo tipoVeiculo) {
+      this.tipoVeiculo = tipoVeiculo;
+   //   Veiculo tipo = Veiculo.PEQUENO;
+   //   Cliente tipo = Cliente.valueOf(tipoCliente); 
+         switch (tipoVeiculo) {
+         case PEQUENO:
+            valorDiaria = new BigDecimal(100.00);
+         break;
+         case MEDIO:
+            valorDiaria = new BigDecimal(150.00);
+         break;
+         case SUV:
+            valorDiaria = new BigDecimal(200.00);
+         break;
+         default: 
+            throw new IllegalArgumentException("Somente PEQUENO, MEDIO e SUV são veículos válidos.");
+      //      System.out.println("Escolha PEQUENO, MEDIO E SUV APENAS");
+         }
+         return valorDiaria;
+      }
+}
+
             break;
           case SUV:
             if (quantidadeDeDias > 3) {
